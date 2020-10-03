407a3302fa13092d21d7672df66e0620bc90fb9b
ymerkli <ymerkli@student.ethz.ch>
2020-10-03 11:30:52 +0200

    task1: add base files

---
+++ task1/solution.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+THRESHOLD = 0.5
+W1 = 1
+W2 = 20
+W3 = 100
+W4 = 0.01
+
+
+def cost_function(true, predicted):
+    """
+        true: true values in 1D numpy array
+        predicted: predicted values in 1D numpy array
+
+        return: float
+    """
+    cost = (true - predicted) ** 2
+
+    # true above threshold (case 1)
+    mask = true > THRESHOLD
+    mask_w1 = np.logical_and(predicted > true, mask)
+    mask_w2 = np.logical_and(np.logical_and(predicted < true, predicted > THRESHOLD), mask)
+    mask_w3 = np.logical_and(predicted < THRESHOLD, mask)
+
+    cost[mask_w1] = cost[mask_w1] * W1
+    cost[mask_w2] = cost[mask_w2] * W2
+    cost[mask_w3] = cost[mask_w3] * W3
+
+    # true value below threshold (case 2)
+    mask = true <= THRESHOLD
+    mask_w1 = np.logical_and(predicted > true, mask)
+    mask_w2 = np.logical_and(predicted < true, mask)
+
+    cost[mask_w1] = cost[mask_w1] * W1
+    cost[mask_w2] = cost[mask_w2] * W2
+
+    # reward for correctly identified safe regions
+    reward = W4 * np.logical_and(predicted <= THRESHOLD, true <= THRESHOLD)
+
+    return np.mean(cost) - np.mean(reward)
+
+
+"""
+Fill in the methods of the Model. Please do not change the given methods for the checker script to work.
+You can add new methods, and make changes. The checker script performs:
+
+
+    M = Model()
+    M.fit_model(train_x,train_y)
+    prediction = M.predict(test_x)
+
+It uses predictions to compare to the ground truth using the cost_function above.
+"""
+
+
+class Model():
+
+    def __init__(self):
+        """
+            TODO: enter your code here
+        """
+        pass
+
+    def predict(self, test_x):
+        """
+            TODO: enter your code here
+        """
+        ## dummy code below 
+        y = np.ones(test_x.shape[0]) * THRESHOLD - 0.00001
+        return y
+
+    def fit_model(self, train_x, train_y):
+        """
+             TODO: enter your code here
+        """
+        pass
+
+
+def main():
+    train_x_name = "train_x.csv"
+    train_y_name = "train_y.csv"
+
+    train_x = np.loadtxt(train_x_name, delimiter=',')
+    train_y = np.loadtxt(train_y_name, delimiter=',')
+
+    # load the test dateset
+    test_x_name = "test_x.csv"
+    test_x = np.loadtxt(test_x_name, delimiter=',')
+
+    M = Model()
+    M.fit_model(train_x, train_y)
+    prediction = M.predict(test_x)
+
+    print(prediction)
+
+
+if __name__ == "__main__":
+    main()
